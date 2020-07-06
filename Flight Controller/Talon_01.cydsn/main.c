06fc49fccc7af774411ae62362c708080ec2b09f
Nathan Miller <nmiller.gsd@gmail.com>
2020-07-06 16:33:32 -0400

    Removed BW analysis stuff from Master/main.c

--- Flight Controller/Talon_01.cydsn/main.c
+++ Flight Controller/Talon_01.cydsn/main.c
@@ -1,11 +1,11 @@
 /* ========================================
-Last edited 05/7/2020
+Last edited 07/6/2020
  * File Name: "main.c" 
  * Project  : Talon_01
  *
  * Author:  Nathan Miller
  *
- * Version: 2.0.1
+ * Version: 2.0.2
  *
  * Description:
  *    Integration Testing. Starting with integration of HC12 project and 
@@ -195,10 +195,6 @@ int main(void)
     uint16 rxCommandStartTime = 0;
     char dataArray[512];
 	char GPS_String[100];
-    char timingArray[100]; //DEBUG
-    uint16 time0, time1, timespan, downtime; //DEBUG
-    uint32 timespan_us[256], downtime_us, timespan_avg_us; //DEBUG
-    uint8 filt_index=255; //DEBUG;
     
     BMP280_U32_t Altitude, Pressure; //x.xx *100
     BMP280_S32_t Temperature, RelativeAltitude; //x.xx *100
@@ -245,16 +241,9 @@ int main(void)
             Temperature = altPtr[2];
             Temperature = (Temperature *9)/5 + 3200; //Farenheit conversion
             
-            time0 = Timer_SW_ReadCounter(); //DEBUG
-            downtime = (time1 - time0)-25/24; downtime_us = (downtime *24*125) /3000; //DEBUG
-            time0 = Timer_SW_ReadCounter(); //DEBUG
-            //IMU_EulersRefresh();
-            //IMU_AccelRefresh();
-            //IMU_GetTemp();
-            time1 = Timer_SW_ReadCounter(); //DEBUG
-            timespan = (time0 - time1)-25/24; timespan_us[++filt_index] = (timespan *24*125) /3000; //DEBUG
-            timespan_avg_us = avg(timespan_us,256);
-            //GPS_Refresh();
+            IMU_EulersRefresh();
+            IMU_AccelRefresh();
+            IMU_GetTemp();
             
             PPS_count = Counter_PPS_ReadCounter();
         }// End if refreshTimer
@@ -263,11 +252,6 @@ int main(void)
         {
             refreshCounter = 0;
             
-            //Write the comma seperated vector to save and send   
-            sprintf(timingArray,"IMU: %li,%li,%li \tIMU_Read_Time: %lu us\tDownTime: %lu us \t\tIMU_AVG_Read = %lu us\r\n",
-                getInt(PITCH,TYPENAME_BNO055_S16), getInt(ROLL,TYPENAME_BNO055_S16), getInt(YAW,TYPENAME_BNO055_U16),
-                timespan_us[filt_index], downtime_us, timespan_avg_us);
-
                 
             //Write the comma seperated vector to save and send   
             sprintf(dataArray,"%u,%li,%li,%li,%li,%li,%li,%li,%lu.%.2u,%li.%.2u,%lu.%.2u,%li.%.2u,",
@@ -304,10 +288,10 @@ int main(void)
             }
             else
                 strcpy(gpsArray,"0,0,0,0,0,0,0,0,0");
-            UART_HC12_PutString(timingArray); //DEBUG
+            
             //send it!
-            //UART_HC12_PutString(dataArray);
-            //UART_HC12_PutString(gpsArray);
+            UART_HC12_PutString(dataArray);
+            UART_HC12_PutString(gpsArray);
             UART_HC12_PutString("\r\n\0");
             
             //Add control Data
